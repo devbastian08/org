b9eeee8896cb1c2808d85a87d9b71462377f6728
devbastian08 <sebastian08.dev@gmail.com>
2023-05-03 20:15:17 -0500

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return;
+    <img src="../public/img/Header.png" alt="header" />;
+  }
+}
+
+export default Header;
