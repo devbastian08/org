92b3812ab342aff394e2e86517684f0f0d6cfff7
devbastian08 <sebastian08.dev@gmail.com>
2023-05-29 21:54:26 -0500

    29-5-2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Formulario from "./components/Formulario/Formulario";
 import MiOrg from "./components/MiOrg";
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Formulario />
-        <MiOrg />
-      </div>
-    );
-  }
+
+function App() {
+  const [mostrarForm, actualizarMostrar] = useState(true);
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarForm);
+  };
+  return (
+    <div>
+      <Header />
+      {mostrarForm && <Formulario />}
+      {/*mostrarForm === true ? <Formulario /> : <div />*/}
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+//import { useState } from "react";
 import "./MiOrg.css";
 
-const MiOrg = () => {
+const MiOrg = (props) => {
   return (
     <section className="orgsection">
-      <h3>Mi Organizacion</h3>
-      <img src="/img/add.png" alt="add" />
+      <h3 className="title">Mi Organizacion </h3>
+      <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
     </section>
   );
 };
